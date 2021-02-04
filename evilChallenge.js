bef1751f8dfee2bb66401ef8acc47ec4201f7e40
SalwaBaqer <s.baqer@hotmail.com>
2021-02-04 18:12:35 +0300

    good luck legends

---
+++ evilChallenge.js
@@ -0,0 +1,32 @@
+let floors = 4; //how many floors
+
+buildTower(floors); //function call
+
+//print the return here using iteration method :D
+
+/** 
+function recieves number of floors to build a tower
+the function returns an array as the following
+
+e.g.
+*****[tower of 1 floor]*****
+[
+  '*'
+]
+*****[tower of 3 floors]*****
+[
+  '  *  ', 
+  ' *** ', 
+  '*****'
+]
+*****[tower of 6 floors]*****
+[
+  '     *     ', 
+  '    ***    ', 
+  '   *****   ', 
+  '  *******  ', 
+  ' ********* ', 
+  '***********'
+]
+*/
+function buildTower(nFloors) {}
